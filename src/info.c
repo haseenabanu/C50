89da9d71189413ef8ef0c9107b3ec6213480faf1
haseenabanu <hazeenabanu66@gmail.com>
2017-02-10 17:41:00 +0530

    Update info.c

--- src/info.c
+++ src/info.c
@@ -98,8 +98,9 @@ double TotalInfo(double V[], DiscrValue MinVal, DiscrValue MaxVal)
 	N = V[v];
 	Sum +=(pow(N,alpha));
 	TotalCases += N;
-    	count[i] += (GEnv.Freq[MaxVal][v]-GEnv.Freq[MinVal][v])/TotalCases;
+    	count[i] += (GEnv.Freq[MaxVal][v]-GEnv.Freq[MinVal][v]);
     }
+	count[i]=count[i]/TotalCases;
 	count1 += count[i];
 	cf=count[i]/count1;
 	//Sum =1-Sum;
